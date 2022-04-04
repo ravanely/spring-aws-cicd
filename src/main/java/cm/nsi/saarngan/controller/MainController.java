3bbfb0deab4d0f1460e0fb4e9c9223b5966f3cba
ravanely <nagueravanely@gmail.com>
2022-04-04 14:04:17 +0100

    add pipeline workflow and change default port

---
+++ src/main/java/cm/nsi/saarngan/controller/MainController.java
@@ -0,0 +1,22 @@
+package cm.nsi.saarngan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * created by : Ravanely
+ * create at : 04/04/2022, 13:44, lun.
+ * saar-ngan
+ **/
+@Controller
+@RequestMapping("")
+public class MainController {
+
+    @GetMapping
+    @ResponseBody
+    public String home() {
+        return "Hello World";
+    }
+}
