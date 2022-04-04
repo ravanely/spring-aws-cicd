0d67a8d09468ba41a7e27537a1069774ed4f7f10
ravanely <nagueravanely@gmail.com>
2022-04-04 13:43:52 +0100

    initial commit

---
+++ src/main/java/cm/nsi/saarngan/SaarNganApplication.java
@@ -0,0 +1,13 @@
+package cm.nsi.saarngan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaarNganApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaarNganApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cm/nsi/saarngan/SaarNganApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.nsi.saarngan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaarNganApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
