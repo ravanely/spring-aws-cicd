2f6e67c81bad51756a9af8435838605271dc486b
ravanely <nagueravanely@gmail.com>
2022-04-06 12:57:01 +0100

    Update pom and main controller

--- src/main/java/cm/nsi/saarngan/controller/MainController.java
+++ src/main/java/cm/nsi/saarngan/controller/MainController.java
@@ -2,8 +2,6 @@ package cm.nsi.saarngan.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * created by : Ravanely
