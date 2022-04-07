a7971a58ed6f8258d6a0489bd8a0e7e6eff4b4bd
ravanely <nagueravanely@gmail.com>
2022-04-07 13:02:59 +0100

    Create some users on startup and update user and category

--- src/main/java/cm/nsi/saarngan/bootstrap/SaarLoader.java
+++ src/main/java/cm/nsi/saarngan/bootstrap/SaarLoader.java
@@ -4,20 +4,27 @@ import cm.nsi.saarngan.entity.Role;
 import cm.nsi.saarngan.entity.User;
 import cm.nsi.saarngan.repository.RoleRepository;
 import cm.nsi.saarngan.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import cm.nsi.saarngan.services.UserService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 /**
  * @author : macbookpro
  * @mailto : nagueravanely@gmail.com
  * @created : 06/04/2022, 20:29 mercredi
  **/
+@Component
 public class SaarLoader implements CommandLineRunner {
 
-    @Autowired
-    private UserRepository userRepo;
-    @Autowired
-    private RoleRepository roleRepo;
+    private final UserRepository userRepo;
+    private final RoleRepository roleRepo;
+    private final UserService userService;
+
+    public SaarLoader(UserRepository userRepo, RoleRepository roleRepo, UserService userService) {
+        this.userRepo = userRepo;
+        this.roleRepo = roleRepo;
+        this.userService = userService;
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -26,27 +33,32 @@ public class SaarLoader implements CommandLineRunner {
             Role adminRole = roleRepo.save(new Role("Admin", "Manage Everything"));
             User adminUser = new User("nam@codejava.net", "nam2020", "Nam", "Ha Minh");
             adminUser.addRole(adminRole);
-            userRepo.save(adminUser);
+            adminUser.setEnabled(true);
+            userService.save(adminUser);
 
             Role salesPersonRole = roleRepo.save(new Role("SalesPerson", "Manage product price, customers, shipping, orders and sales report"));
             User salesPersonUser = new User("toto@toto.cm", "toto2020", "Toto", "toto");
             salesPersonUser.addRole(salesPersonRole);
-            userRepo.save(salesPersonUser);
+            salesPersonUser.setEnabled(true);
+            userService.save(salesPersonUser);
 
-            Role EditorRole = roleRepo.save(new Role("Editor", "Manage categories, brands, products, articles and menus"));
-            User EditorUser = new User("tata@tata.cm", "tata2020", "Tata", "tata");
-            salesPersonUser.addRole(EditorRole);
-            userRepo.save(EditorUser);
+            Role editorRole = roleRepo.save(new Role("Editor", "Manage categories, brands, products, articles and menus"));
+            User editorUser = new User("tata@tata.cm", "tata2020", "Tata", "tata");
+            editorUser.addRole(editorRole);
+            editorUser.setEnabled(true);
+            userService.save(editorUser);
 
             Role shipperRole = roleRepo.save(new Role("Shipper", "View products, view orders and update order status"));
-            User shipperUser = new User("tata@tata.cm", "tata2020", "Tata", "tata");
-            salesPersonUser.addRole(shipperRole);
-            userRepo.save(shipperUser);
+            User shipperUser = new User("lola@lola.cm", "lola2020", "Lola", "lola");
+            shipperUser.addRole(shipperRole);
+            shipperUser.setEnabled(true);
+            userService.save(shipperUser);
 
             Role assistantRole = roleRepo.save(new Role("Assistant", "Manage questions and reviews"));
             User assistantUser = new User("zozo@zozo.cm", "zozo2020", "Zozo", "zozo");
-            salesPersonUser.addRole(assistantRole);
-            userRepo.save(assistantUser);
+            assistantUser.addRole(assistantRole);
+            assistantUser.setEnabled(true);
+            userService.save(assistantUser);
         }
     }
 }
